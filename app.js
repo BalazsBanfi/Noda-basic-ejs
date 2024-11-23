32c310b524311390547b9248808fe4ce9ce81c29
bally4h <banfibalazs81@gmail.com>
2024-11-23 14:49:09 +0100

    Add footer

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.get("/", (req, res) => {
   res.render("index", { links: links, users: users });
 });
 
+app.get("/about", (req, res) => {
+  res.render("about", { links: links });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`My first Express app - listening on port ${PORT}!`);
